1a341eb2520c6a9a2dc92d9e0b4ceb3a0aad0aa2
ligtinho <merrauq@gmail.com>
2024-01-22 09:14:51 -0300

    Versão completa

--- src/main/java/healthplan/beneficios/config/SecurityConfig.java
+++ src/main/java/healthplan/beneficios/config/SecurityConfig.java
@@ -14,39 +14,53 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Configuração de autenticação (usuário em memória para exemplo)
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password(passwordEncoder().encode("admin"))
-            .roles("USER");
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Configuração de autenticação (usuário em memória para exemplo)
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).roles("USER");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // Configuração de autorização
-    	http.csrf().disable();
-        http.authorizeRequests()
-            .antMatchers("/api/beneficiarios/**").hasRole("USER")
-            .antMatchers(HttpMethod.POST, "/api/beneficiarios/**").hasRole("USER")
-            .antMatchers(HttpMethod.PUT, "/api/beneficiarios/**").hasRole("USER")
-            .antMatchers(HttpMethod.DELETE, "/api/beneficiarios/**").hasRole("USER")
-            .and()
-            .httpBasic(); // Utilize a autenticação básica (usuário e senha) para testes
-        
-     // Adiciona logs para depuração
-        http.exceptionHandling().accessDeniedHandler((request, response, accessDeniedException) -> {
-            System.out.println("Acesso negado: " + accessDeniedException.getMessage());
-        });
-        
-        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
-            System.out.println("Falha na autenticação: " + authException.getMessage());
-        });
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Configuração de autorização
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/api/beneficiarios/**").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/api/beneficiarios/**").hasRole("USER")
+				.antMatchers(HttpMethod.PUT, "/api/beneficiarios/**").hasRole("USER")
+				.antMatchers(HttpMethod.DELETE, "/api/beneficiarios/**").hasRole("USER")
+				.antMatchers("/home").authenticated()
+				.antMatchers("/create").authenticated()
+				.antMatchers("/update").authenticated()
+				.antMatchers("/listarbeneficiarios").authenticated()
+				.antMatchers("/listardocumentos").authenticated()
+				.and()
+				.formLogin()
+				.loginProcessingUrl("/perform_login") 
+				.defaultSuccessUrl("/home", true) // Redireciona para "/home" em caso de login bem-sucedido
+				.failureUrl("/?error=true") // Adiciona o parâmetro error na URL em caso de falha no login
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutUrl("/sair")
+				.logoutSuccessUrl("/?logout")
+				.permitAll()
+				.and()
+				.httpBasic();
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+		// Adiciona logs para depuração
+		http.exceptionHandling().accessDeniedHandler((request, response, accessDeniedException) -> {
+			System.out.println("Acesso negado: " + accessDeniedException.getMessage());
+		});
+
+		http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+			System.out.println("Falha na autenticação: " + authException.getMessage());
+		});
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
